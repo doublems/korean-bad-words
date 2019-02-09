d120a0a64aeb937b7c35cf0fe8d762963b13f4d9
computerphilosopher <abc3242@gmail.com>
2019-02-09 21:27:53 +0900

    db화 완료

--- add_tool.py
+++ add_tool.py
@@ -8,11 +8,18 @@ file = open("./to_add.md", 'r', encoding = "UTF8")
 to_add = [line.rstrip() for line in file] 
 
 for word in to_add:
-    query = "select \"" + word + "\" from bad_words"
-    cursor.execute(query)
+    select_query = "select word from bad_words where word = \"" + word + "\""
+    cursor.execute(select_query)
 
-r = cursor.fetchall()
-print(r)
+    exist = cursor.fetchone()
+    print(exist, end = " ")
+
+    if not exist:
+        insert_query = "insert into bad_words (word) values(?)"  
+        cursor.execute(insert_query, (word,) )
+        connection.commit()
+
+connection.commit()
+connection.close()
 
 file.close()
- #to_add.close()
