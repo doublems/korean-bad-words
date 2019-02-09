4682bdff1f6c0b5dd6698d4c3cea1027aab22211
computerphilosopher <abc3242@gmail.com>
2019-02-09 17:11:19 +0900

    sqlite 검색 성공

--- add_tool.py
+++ add_tool.py
@@ -8,12 +8,11 @@ file = open("./to_add.md", 'r', encoding = "UTF8")
 to_add = [line.rstrip() for line in file] 
 
 for word in to_add:
-    query = "select " + word + " from bad_words"
-    #print(query)
+    query = "select \"" + word + "\" from bad_words"
     cursor.execute(query)
-    r = cursor.fetchall()
-    #print(word, end ="")
 
+r = cursor.fetchall()
+print(r)
 
 file.close()
  #to_add.close()
